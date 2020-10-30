7cb14fc87eccd46492d2ede3642b3ba4e75e6a56
Ndifreke Friday <ndiecodes@gmail.com>
2020-10-30 09:13:16 +0100

    add google adsense

--- config.js
+++ config.js
@@ -1,49 +1,52 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://ndiecodes.com',
-  pathPrefix: '/',
-  title: 'Blog by Ndie',
-  subtitle: 'Articles on Software Development. Written by Ndifreke Friday, Software Developer and  Human',
-  copyright: '© All rights reserved.',
-  disqusShortname: 'ndiecodes',
+  url: "https://ndiecodes.com",
+  pathPrefix: "/",
+  title: "Blog by Ndie",
+  subtitle:
+    "Articles on Software Development. Written by Ndifreke Friday, Software Developer and  Human",
+  copyright: "© All rights reserved.",
+  disqusShortname: "ndiecodes",
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-156217691-1',
+  googleAnalyticsId: "UA-156217691-1",
+  googleAdsenseId: "ca-pub-8436677894143158",
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/",
     },
     {
-      label: 'tags',
-      path: '/tags'
+      label: "tags",
+      path: "/tags",
     },
     {
-      label: 'About me',
-      url: 'https://ndiecodes.github.io/'
+      label: "About me",
+      url: "https://ndiecodes.github.io/",
     },
   ],
   author: {
-    name: 'Ndifreke Friday',
-    photo: '/photo.jpg',
-    bio: 'Articles on software development; things I am working on or interested in.',
+    name: "Ndifreke Friday",
+    photo: "/photo.jpg",
+    bio:
+      "Articles on software development; things I am working on or interested in.",
     contacts: {
-      email: '',
-      facebook: 'ndiecodes',
-      telegram: 'ndiecodes',
-      twitter: 'ndiecodes',
-      github: 'ndiecodes',
-      rss: '',
-      vkontakte: '',
-      linkedin: 'ndiecodes',
-      instagram: 'ndiecodes',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-    }
-  }
+      email: "",
+      facebook: "ndiecodes",
+      telegram: "ndiecodes",
+      twitter: "ndiecodes",
+      github: "ndiecodes",
+      rss: "",
+      vkontakte: "",
+      linkedin: "ndiecodes",
+      instagram: "ndiecodes",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+    },
+  },
 };
